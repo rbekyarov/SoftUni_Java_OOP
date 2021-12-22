f1305340681ada9b3bcc849ac53c6ba4b8458896
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-22 23:32:54 +0200

    Working With Abstraction

--- src/OOP_01_Working_With_Abstraction/p1RhombusOfStars.java
+++ src/OOP_01_Working_With_Abstraction/p1RhombusOfStars.java
@@ -7,8 +7,8 @@ public class p1RhombusOfStars {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
-        for (int row = 1; row <= n; row++) {
+        //print top
+        for (int row = 0; row < n; row++) {
 
             for (int i = 0; i < n - row; i++) {
                 System.out.print(" ");
@@ -18,6 +18,14 @@ public class p1RhombusOfStars {
             }
             System.out.println();
         }
+
+        //print middle
+        for (int i = 0; i < n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+
+        //print bottom
         for (int row = 1; row <= n - 1; row++) {
             for (int i = 0; i < row; i++) {
                 System.out.print(" ");
