80fcc5c04c0d6f1aca3760dfc99e52581afe7e52
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-22 22:54:18 +0200

    Working With Abstraction

---
+++ src/OOP_01_Working_With_Abstraction/p1RhombusOfStars.java
@@ -0,0 +1,12 @@
+package OOP_01_Working_With_Abstraction;
+
+import java.util.Scanner;
+
+public class p1RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+    }
+}
