ebd918e95e4c22f60fe79ef24121e9712510f539
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-03-05 20:15:15 +0200

    Interfaces_and_Abstraction

--- src/main/java/OOP_04_Exercise_Encapsulation/P4PizzaCaloriesNOTFINISH/Pizza.java
+++ src/main/java/OOP_04_Exercise_Encapsulation/P4PizzaCaloriesNOTFINISH/Pizza.java
@@ -1,5 +1,4 @@
-package OOP_04_Exercise_Encapsulation.P4PizzaCaloriesNOTFINISH;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pizza {
@@ -7,12 +6,16 @@ public class Pizza {
     private Dough dough;
     private List<Topping> toppings;
     private int numberOfToppings;
-    public Pizza(String name, int numberOfToppings){
-        this.name=name;
 
+    public Pizza(String name, int numberOfToppings) {
+        setName(name);
+        setToppings(numberOfToppings);
+        toppings = new ArrayList<>();
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
+        if (name.trim().isEmpty() || name.length() > 15)
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
         this.name = name;
     }
 
@@ -20,9 +23,29 @@ public class Pizza {
         this.dough = dough;
     }
 
-    public void setToppings() {
-        this.toppings = toppings;
+    private void setToppings(int numberOfToppings) {
+        if (numberOfToppings < 0 || numberOfToppings > 10)
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        this.numberOfToppings = numberOfToppings;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    public double getOverallCalories() {
+        double calories = 0;
+        calories += dough.calculateCalories();
+        for (Topping topping : toppings)
+            calories += topping.calculateCalories();
+        return calories;
+    }
+
+    public String toString() {
+        return String.format("%s - %.2f", getName(), getOverallCalories());
+    }
 }
--- src/main/java/OOP_04_Exercise_Encapsulation/P4PizzaCaloriesNOTFINISH/Topping.java
+++ src/main/java/OOP_04_Exercise_Encapsulation/P4PizzaCaloriesNOTFINISH/Topping.java
@@ -1,22 +1,40 @@
-package OOP_04_Exercise_Encapsulation.P4PizzaCaloriesNOTFINISH;
-
 public class Topping {
     private String toppingType;
-    private double weight;
+    private double weight, modifier;
 
     public Topping(String toppingType, double weight) {
-        this.toppingType = toppingType;
-        this.weight = weight;
+        setToppingType(toppingType);
+        setWeight(weight);
     }
 
     private void setToppingType(String toppingType) {
+        switch (toppingType) {
+            case "Meat":
+                modifier = 1.2;
+                break;
+            case "Veggies":
+                modifier = 0.8;
+                break;
+            case "Cheese":
+                modifier = 1.1;
+                break;
+            case "Sauce":
+                modifier = 0.9;
+                break;
+            default:
+                throw new IllegalArgumentException(String.format(
+                        "Cannot place %s on top of your pizza.", toppingType));
+        }
         this.toppingType = toppingType;
     }
 
     private void setWeight(double weight) {
+        if (weight < 1 | weight > 50) throw new IllegalArgumentException(String.format(
+                "%s weight should be in the range [1..50].", toppingType));
         this.weight = weight;
     }
-    public double calculateCalories (){
-        return 0;
+
+    public double calculateCalories() {
+        return 2 * modifier * weight;
     }
 }
