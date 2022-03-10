90dd2fba5fec4e5a32b8ea5d6627a99572f5a4c3
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-03-10 23:36:22 +0200

    Polymorphism

--- src/main/java/OOP_10_Exercise_Polymorphism/p3/Main.java
+++ src/main/java/OOP_10_Exercise_Polymorphism/p3/Main.java
@@ -1,36 +1,38 @@
 package OOP_10_Exercise_Polymorphism.p3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Animal animal = null;
+        List<Animal> animals = new ArrayList<>();
         Food food = null;
         String input = scanner.nextLine();
         int countInputLine = 0;
-        while (!input.equals("End")){
+        while (!input.equals("End")) {
             countInputLine++;
-            String[]inputLine = input.split(" ");
-            if (countInputLine%2==1){
-             switch (inputLine[0]){
-                 case "Cat":
-                     animal = new Cat(inputLine[0],inputLine[1],Double.parseDouble(inputLine[2]),inputLine[3],inputLine[4]);
-                     break;
-                 case "Tiger":
-                     animal = new Tiger(inputLine[0],inputLine[1],Double.parseDouble(inputLine[2]),inputLine[3]);
+            String[] inputLine = input.split(" ");
+            if (countInputLine % 2 == 1) {
+                switch (inputLine[0]) {
+                    case "Cat":
+                        animals.add(new Cat(inputLine[0], inputLine[1], Double.parseDouble(inputLine[2]), inputLine[3], inputLine[4]));
+                        break;
+                    case "Tiger":
+                        animals.add(new Tiger(inputLine[0], inputLine[1], Double.parseDouble(inputLine[2]), inputLine[3]));
 
-                     break;
-                 case "Zebra":
-                     animal = new Zebra(inputLine[0],inputLine[1],Double.parseDouble(inputLine[2]),inputLine[3]);
+                        break;
+                    case "Zebra":
+                        animals.add(new Zebra(inputLine[0], inputLine[1], Double.parseDouble(inputLine[2]), inputLine[3]));
 
-                     break;
-                 case "Mouse":
-                     animal = new Mouse(inputLine[0],inputLine[1],Double.parseDouble(inputLine[2]),inputLine[3]);
-                     break;
-             }
-            }else {
-                switch (inputLine[0]){
+                        break;
+                    case "Mouse":
+                        animals.add(new Mouse(inputLine[0], inputLine[1], Double.parseDouble(inputLine[2]), inputLine[3]));
+                        break;
+                }
+            } else {
+                switch (inputLine[0]) {
                     case "Vegetable":
                         food = new Vegetable(Integer.parseInt(inputLine[1]));
                         break;
@@ -41,8 +43,11 @@ public class Main {
             }
             input = scanner.nextLine();
         }
-        animal.makeSound();
-        animal.eat(food);
-        System.out.println(animal);
+        for (Animal animal : animals) {
+            animal.makeSound();
+            animal.eat(food);
+            System.out.println(animal);
+        }
+
     }
 }
